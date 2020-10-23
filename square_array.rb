088f117a44d6e7513d03655768822291f298c512
Mnkith <munkithabid@gmail.com>
2020-10-23 00:40:21 -0500

    commit tree 903b3a3200898255254699cfc339f2b79f1461b3

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,22 @@
+#with collect, this will never be evaluated
 def square_array(array)
-  # your code here
+  array.collect {|num| num**2}
 end
+#last method in file is alaways the one evaluated by spec test
+def square_array(array)
+  index = 0
+  array.each do |num|
+    array[index] = num**2
+    index += 1
+  end
+end
+
+# def square_array(array)
+#   index = 0
+#   array2 = []
+#   array.each do |num|
+#     array2[index] = num**2
+#     index += 1
+#   end
+#   array2
+# end
